e4e7606a3bdbba8d2e63032e06894edbfa08bfa8
jamtesdemx <quan.wu@imaginanet.com>
2022-10-09 21:27:03 +0200

    the first changes

--- src/app/login/login/login.component.spec.ts
+++ src/app/login/login/login.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+// Http testing module and mocking controller
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit, QueryList } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { select, Store } from '@ngrx/store';
+import { from, Observable } from 'rxjs';
 import { Quote } from 'src/app/domain/quote.model';
 import { QuoteSevice } from 'src/app/services/quote.service';
+import { getQuoteContentState } from 'src/app/store';
+import { QUOTE_LOAD, QUOTE_SUCCESS } from 'src/app/store/actions/quote.action';
+import {LOGIN} from 'src/app/store/actions/auth.action';
+
+import * as fromRoot from '../../store/reducers/quote.reducer'
 
 
 @Component({
@@ -14,15 +21,24 @@ export class LoginComponent implements OnInit {
 
   quotes!:Quote[];
   quote!:Quote;
+  quote$!: Observable<{quote:Quote}>
   form!: FormGroup;
-  constructor(private quoteService$:QuoteSevice, private fb: FormBuilder) { 
-    
-    this.quoteService$.getQuote().subscribe(q=>{
-      this.quotes = q.quotes
-      this.quote = this.quotes[Math.floor(Math.random()*10)]
-      console.log(this.quote)
+  constructor(private quoteService$:QuoteSevice, private fb: FormBuilder, private store$: Store<fromRoot.State>) { 
 
-    } )
+    console.log(this.store$);
+    this.quote$= this.store$.pipe(select("quote")) as unknown as Observable<{quote:Quote}>
+    this.quote$.subscribe(console.log)
+    this.store$.dispatch(QUOTE_LOAD())
+    
+    // this.quoteService$.getQuote().subscribe(q=>{
+    //   this.quotes = q.quotes;
+    //   this.quote = this.quotes[Math.floor(Math.random()*10)]
+    //   this.store$.dispatch(QUOTE_SUCCESS(this.quote))
+    //    this.store$.select(getQuoteContentState).subscribe(val=>console.log("getQuoteContentState",val))
+    //   this.quote$.subscribe(val=>console.log(val))
+    //   console.log(this.quote)
+
+    // } )
   }
 
   ngOnInit(): void {
@@ -37,19 +53,20 @@ export class LoginComponent implements OnInit {
 
   onSubmit(form:any, event:any){
     event.preventDefault();
-      console.log(JSON.stringify(form.value));
-      console.log(form.valid);
+      console.log(form,event);
+      // console.log(form.valid);
      // this.form.controls['email'].setValidators(this.validate);
+     this.store$.dispatch(LOGIN(form?.value))
   }
 
-  validate(c:FormControl):{[key:string]:any}{
+  validate(c:any):{[key:string]:any} | null{
     if(!c.value){
       return {null:null};
     }
     const pattern =/^j.*$/;
       if(pattern.test(c.value)){
         console.log(c.value);
-        return {emailIsValid:true};
+        return null;
       }else return {
         emailNotValid : 'the email must start with j_'
       }
